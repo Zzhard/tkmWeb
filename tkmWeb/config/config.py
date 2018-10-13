280c7acc614a54bedacecd08541510ffa5bab35e
Zzhard <32160561+Zzhard@users.noreply.github.com>
2018-10-13 14:45:26 +0800

    Update config.py

--- tkmWeb/config/config.py
+++ tkmWeb/config/config.py
@@ -10,16 +10,16 @@ url=''
 username='13193205886'
 password1='123456'
 #数据库配置
-host='aeasydb01.mysql.rds.aliyuncs.com'
-user='p2pdev'
-password='p2pdev'
-database='p2psys_temp'
+host=''
+user=''
+password=''
+database=''
 port=3306
 #邮箱配置
-sender='1178145190@qq.com'#发送方
-receiver='1178145190@qq.com' #接收方
-emailusername='1178145190@qq.com'#登录邮箱用户名
-emailpassword='zxuhgazkykxwiedj'#登录邮箱密码
+sender='@qq.com'#发送方
+receiver='@.com' #接收方
+emailusername='@qq.com'#登录邮箱用户名
+emailpassword=''#登录邮箱密码
 server='smtp.qq.com' #smtp服务器
 #项目目录
 basedir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
